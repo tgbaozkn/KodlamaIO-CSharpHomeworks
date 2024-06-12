c3ed04555ac05a03c4c957f2f818c08cf744e1fe
Tugba <39098806+tgbaozkn@users.noreply.github.com>
2024-06-12 16:49:10 +0300

    Add files via upload

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FourCSharpProject.Business.Concrete
+{
+	public class CategoryManager
+	{
+		public CategoryManager()
+		{
+		}
+	}
+}
+
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+
+            //veri erişimi  çağırmak gerekiyor.
+            //bir iş sınıfı başka sınıfları newlemez.
+            //belli kurallar var sonra bu fonksiyon
+            return _productDal.GetAll(); //dependency injection
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >=min && p.UnitPrice <=max);
+        }
+    }
+}
+
